0aca08e919b93dd6e370a72344f40c46d4d8e165
tanglei <tanglei_0315@163.com>
2018-03-28 18:01:18 +0800

    cmdb_client_20180327

--- index.py
+++ index.py
@@ -31,8 +31,9 @@ server_info = {
 if __name__ == '__main__':
     #times = 0
     #while True:
-        RequestData = {'data':server_info}
-        RequestData = json.dumps(RequestData)
+        #RequestData = {'data':server_info}
+        #RequestData = json.dumps(RequestData)
+        RequestData = urllib.urlencode({'data':server_info})
         result = RequestUrl('127.0.0.1','9000','/receive_server_info/',RequestData,30)
         print '======第%d次请求，结果为: %s========= %(times,result)'
         #times += 1
